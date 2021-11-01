b018b4db827a469826c0f583453213a708924e96
Joe McPartland <joemcpartland@gmail.com>
2021-11-01 10:08:55 -0600

    commit tree fdde1ea78f584562577bd8ca66e34eb0819d951f

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -4,3 +4,9 @@ debugger
 let x = 99
 debugger
 console.log(x)
+
+let oo = { foo: 42, bar: 83, baz: 79 };
+for (let key in oo) {
+  let value = oo[key];
+  console.log("key:", key, "value:", value);
+}
